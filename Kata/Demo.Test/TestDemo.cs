a256f91b7ae0aed6e70d3b327ac044ab3b6dfd09
RetroSabian <srajbully@retrorabbit.co.za>
2019-01-18 11:16:54 +0200

    KATA ADDED

---
+++ Kata/Demo.Test/TestDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo;
+using Kata;
+using NUnit.Framework;
+
+namespace Demo.Test
+{
+   public class TestDemo
+    {
+        private RPS app;
+        public TestDemo()
+        {
+            app = new RPS();
+        }
+
+        [TestCase('R','R',0)]
+        [TestCase('R', 'P', 2)]
+        [TestCase('R', 'S', 1)]
+        [TestCase('R', 'X', 2)]
+        [TestCase('P', 'P', 0)]
+        [TestCase('P', 'R', 1)]
+        [TestCase('P', 'S', 2)]
+        [TestCase('P', 'X', 1)]
+        [TestCase('S', 'S', 0)]
+        [TestCase('S', 'P', 1)]
+        [TestCase('S', 'R', 2)]
+        [TestCase('S', 'X', 2)]
+        [TestCase('X', 'R', 1)]
+        [TestCase('X', 'S', 1)]
+        [TestCase('X', 'P', 2)]
+        public void Fight(char c1, char c2, int res)
+        {
+            //arrange
+            var x = app.play(c1, c2);
+            //act
+            //assert
+            Assert.AreEqual(res, x);
+        }
+
+
+
+    }
+}
---
+++ Kata/Kata/RPS.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class RPS
+    {
+           public RPS()
+        {
+        }
+
+        public int play(char p1, char p2)
+        {
+            int result = -1;
+            if(p1 == 'R' && p2 == 'R')
+            {
+                result = 0;
+            }
+            if (p1 == 'R' && p2=='P' )
+            {
+                result = 2;
+            }
+            if (p1 == 'R' && p2 == 'S')
+            {
+                result = 1;
+            }
+            if (p1 == 'P' && p2 == 'P')
+            {
+                result = 0;
+            }
+            if (p1 == 'P' && p2 == 'R')
+            {
+                result = 1;
+            }
+            if (p1 == 'P' && p2 == 'S')
+            {
+                result = 2;
+            }
+            if (p1 == 'S' && p2 == 'S')
+            {
+                result = 0;
+            }
+            if (p1 == 'S' && p2 == 'R')
+            {
+                result = 2;
+            }
+            if (p1 == 'S' && p2 == 'P')
+            {
+                result = 1;
+            }
+            if (p1 == 'R' && p2 == 'X')
+            {
+                result = 2;
+
+            }
+            if (p1 == 'P' && p2 == 'X')
+            {
+                result = 1;
+            }
+            if (p1 == 'S' && p2 == 'X')
+            {
+                result = 2;
+            }
+            if (p1 == 'X' && p2 == 'R')
+            {
+                result = 1;
+            }
+            if (p1 == 'X' && p2 == 'P')
+            {
+                result = 2;
+            }
+            if (p1 == 'X' && p2 == 'S')
+            {
+                result = 1;
+            }
+            return result;
+        }
+    }
+}
